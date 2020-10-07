17220741b489e4c53b192f64d14c4009c5053969
Floern <dev@floern.com>
2020-10-07 19:59:47 +0200

    ci & release actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,83 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.4.10"
+val artifactGroup = "com.floern.castingcsv"
+val artifactName = "casting-csv-kt"
+val artifactVersion = "1.0"
+val publicationName = "release"
+
+buildscript {
+	dependencies {
+		classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin")
+	}
 }
 
-group = "com.floern.castingcsv"
-version = "1.0"
+plugins {
+	kotlin("jvm") version "1.4.10"
+	id("com.jfrog.bintray") version "1.8.5"
+	id("maven-publish")
+}
+
+group = artifactGroup
+version = artifactVersion
 
 kotlin {
-    explicitApi()
+	explicitApi()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
-    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:0.11.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
+	implementation("com.github.doyaaaaaken:kotlin-csv-jvm:0.11.1")
 
-    testImplementation(kotlin("test-junit"))
+	testImplementation(kotlin("test-junit"))
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+	archiveClassifier.set("sources")
+	from(sourceSets.main.get().allSource)
+}
+
+publishing {
+	publications {
+		create<MavenPublication>(publicationName) {
+			groupId = artifactGroup
+			artifactId = artifactName
+			version = artifactVersion
+			from(components["java"])
+			artifact(sourcesJar)
+		}
+	}
+}
+
+bintray {
+	user = findProperty("bintrayUser") as String?
+	key = findProperty("bintrayKey") as String?
+
+	setPublications(publicationName)
+	publish = true
+
+	pkg.apply {
+        repo = "maven"
+        name = artifactName
+        vcsUrl = "https://github.com/Floern/casting-csv-kt.git"
+        websiteUrl = "https://github.com/Floern/casting-csv-kt"
+        issueTrackerUrl = "https://github.com/Floern/casting-csv-kt/issues"
+		githubRepo = "Floern/casting-csv-kt"
+		githubReleaseNotesFile = "README.md"
+        setLicenses("Apache-2.0")
+		setLabels("kotlin", "csv")
+
+        version.apply {
+            name = artifactVersion
+        }
+    }
 }
